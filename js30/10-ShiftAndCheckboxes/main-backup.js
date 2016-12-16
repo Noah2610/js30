954da910ca730e3948b4688d9b4b93cb016d5599
Noah <noah@rays-lab.com>
2016-12-16 16:02:13 +0100

    10: changed for loop to forEach

--- js30/10-ShiftAndCheckboxes/main-backup.js
+++ js30/10-ShiftAndCheckboxes/main-backup.js
@@ -6,7 +6,6 @@ let lastChecked;
 
 function checkFunc(event) {
 
-	/*
 	for (var count = 0; count < chboxes.length; count++) {
 		if (chboxes[count] == this) {
 
@@ -15,7 +14,8 @@ function checkFunc(event) {
 			}
 
 			else if (event.shiftKey && lastChecked) {
-					let loopIncr = 0;
+				let loopIncr = 0;
+
 				if (count > lastChecked.index) {  // lastChecked is above
 					var loopStart = count - 1;
 					var loopEnd = lastChecked.index - 1;
@@ -37,41 +37,6 @@ function checkFunc(event) {
 			}
 		}
 	}
-	*/
-
-
-
-	chboxes.forEach((checkbox, index) => {
-
-		if (checkbox == this) {
-
-			if (lastChecked != undefined && event.shiftKey) {
-
-					let loopIncr; let loopStart; let loopEnd;
-				if (index > lastChecked) {  // lastChecked is above
-					loopStart = index - 1;
-					loopEnd = lastChecked - 1;
-					loopIncr = -1;
-				} else if (index < lastChecked) {  // lastChecked is below
-					loopStart = index + 1;
-					loopEnd = lastChecked + 1;
-					loopIncr = 1;
-				} else return;
-
-				for (let count = loopStart; count < loopEnd || count > loopEnd; count += loopIncr) {
-						// console.log("TEST");
-					// chboxes[count].checked = true;  // always set checkboxes to true
-					// chboxes[count].checked = !chboxes[count].checked;  // invert checkboxes values (true -> false; false ->)
-					// chboxes[count].checked = this.checked;  // set checkboxes values to lastChecked state (recommended)
-					chboxes[count].checked = chboxes[index].checked;  // set checkboxes values to selected state
-				}
-
-			}
-				lastChecked = index;
-		}
-
-	});
-	
 }
 
 
