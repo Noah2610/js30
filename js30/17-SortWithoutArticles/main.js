84a655fdd168f218c610a99554b78e927949f069
Noah <noah@rays-lab.com>
2016-12-21 15:14:21 +0100

    17 done, removed commented out code

--- js30/17-SortWithoutArticles/main.js
+++ js30/17-SortWithoutArticles/main.js
@@ -2,34 +2,6 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 const list = document.querySelector("#bands");
-/* const articles = ["the", "a", "an"];
-
-let bandsTmp = [];
-
-
-function bandsFunc() {
-	bands.map((band,index) => {
-		
-			let check = false;
-		for (let count = 0; count < articles.length; count++) {
-			if (band.substring(0,articles[count].length).toLowerCase() == articles[count]) {
-				bandsTmp.push(band.substring(articles[count].length + 1));
-				check = true;
-				break;
-			}
-		}
-		if (check == false) {
-			bandsTmp.push(band);
-		}
-
-	});
-
-
-
-	bandsTmp.sort();
-}
-
-bandsFunc(); */
 
 
 function strip(band) {
